45b807ba5a8a51ec7351ba42d364a49d50ec2d37
VincenzoVitello <92366183+VincenzoVitello@users.noreply.github.com>
2022-01-19 15:01:11 +0100

    milestone 3 eseguito (da fixare le date)

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {     //dati
         dataIndex: 0,
+        inputValue: '',
+
         contacts: [
             {
             name: 'Michele',
@@ -12,19 +14,16 @@ var app = new Vue({
                     date: '10/01/2020 15:30:55',
                     text: 'Hai portato a spasso il cane?',
                     status: 'sent',
-                    dropdownActive: false,
                     },
                     {
                     date: '10/01/2020 15:50:00',
                     text: 'Ricordati di dargli da mangiare',
                     status: 'sent',
-                    dropdownActive: false,
                     },
                     {
                     date: '10/01/2020 16:15:22',
                     text: 'Tutto fatto!',
                     status: 'received',
-                    dropdownActive: false,
                     }
                 ],
             },
@@ -37,19 +36,16 @@ var app = new Vue({
                     date: '20/03/2020 16:30:00',
                     text: 'Ciao come stai?',
                     status: 'sent',
-                    dropdownActive: false,
                     },
                     {
                     date: '20/03/2020 16:30:55',
                     text: 'Bene grazie! Stasera ci vediamo?',
                     status: 'received',
-                    dropdownActive: false,
                     },
                     {
                     date: '20/03/2020 16:35:00',
                     text: 'Mi piacerebbe ma devo andare a fare la spesa.',
                     status: 'sent',
-                    dropdownActive: false,
                     }
                 ],
             },
@@ -62,19 +58,16 @@ var app = new Vue({
                     date: '28/03/2020 10:10:40',
                     text: 'La Marianna va in campagna',
                     status: 'received',
-                    dropdownActive: false,
                     },
                     {
                     date: '28/03/2020 10:20:10',
                     text: 'Sicuro di non aver sbagliato chat?',
                     status: 'sent',
-                    dropdownActive: false,
                     },
                     {
                     date: '28/03/2020 16:15:22',
                     text: 'Ah scusa!',
                     status: 'received',
-                    dropdownActive: false,
                     }
                 ],
             },
@@ -87,16 +80,35 @@ var app = new Vue({
                     date: '10/01/2020 15:30:55',
                     text: 'Lo sai che ha aperto una nuova pizzeria?',
                     status: 'sent',
-                    dropdownActive: false,
                     },
                     {
                     date: '10/01/2020 15:50:00',
                     text: 'Si, ma preferirei andare al cinema',
                     status: 'received',
-                    dropdownActive: false,
                     }
                 ],
             },
-        ]            
+        ]           
     }, 
+    methods: {
+        addAndClean: function(){
+            this.contacts[this.dataIndex].messages.push({
+                
+                    
+                    text: this.inputValue,
+                    status: 'sent',
+                    
+            });
+            this.inputValue = "";
+            setTimeout(() => {
+                this.contacts[this.dataIndex].messages.push({
+                
+                    date: '',
+                    text: 'Puppa',
+                    status: 'received',
+                    
+            });
+            }, 1000);
+        }
+    }
 })
